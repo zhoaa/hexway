e4e387dfaad2f241d3cf5ac6af672a44659d1a06
Aaron Zhou <m4m3sh1b4@gmail.com>
2017-10-21 19:21:16 -0400

    Merge settings and hex grid

---
+++ js/main.js
@@ -0,0 +1,28 @@
+//var slider = document.getElementById("myRange");
+//var output = document.getElementById("demo");
+//output.innerHTML = slider.value; // Display the default slider value
+//
+//// Update the current slider value (each time you drag the slider handle)
+//slider.oninput = function () {
+//    output.innerHTML = this.value;
+//}
+
+
+var isPlay = true;
+
+function start() {
+    var startButton = document.getElementById("start-btn");
+    console.log(startButton.style.backgroundImage);
+    if (isPlay) {
+        startButton.style.backgroundImage = "url('../img/pause.png')";
+        isPlay = false;
+    } else {
+        startButton.style.backgroundImage = "url('../img/play.png')";
+        isPlay = true;
+    }
+
+}
+
+function clear() {
+
+}
